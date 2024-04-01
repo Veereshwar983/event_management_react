20f8eb4ea10b5b34013bf3d6ec910c7aacc5e4c4
veereshwar AK <veereshak06@gmail.com>
2024-04-01 17:58:43 +0530

    working css issues

--- src/App.tsx
+++ src/App.tsx
@@ -23,10 +23,8 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Link,
-  // useNavigate,
 } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
+import { AppBar, Toolbar, Typography, Container } from "@mui/material";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
@@ -35,19 +33,20 @@ import { useState } from "react";
 import Logout from "./Logout";
 
 const App = () => {
-  // const navigate = useNavigate();
 
   const [loggedIn, setLoggedIn] = useState(false);
   const handleLogout = () => {
-    // Perform logout actions here, such as clearing user data from local storage or state
     setLoggedIn(false);
-    // Redirect to the login page after logout
-    // history.push("/login");
-    // navigate("/login");
   };
   return (
     <Router>
-      <div>
+      <div
+        style={{
+          position: "relative",
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
         <AppBar position="fixed">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -57,7 +56,14 @@ const App = () => {
             <Logout loggedIn={loggedIn} handleLogout={handleLogout} />
           </Toolbar>
         </AppBar>
-        <Container style={{ paddingTop: "64px" }}>
+
+        <Container
+          style={{
+            paddingTop: "64px",
+            overflow: "hidden",
+            height: "calc(100vh - 64px)",
+          }}
+        >
           <Routes>
             <Route path="/register" element={<Register />} />
             <Route
